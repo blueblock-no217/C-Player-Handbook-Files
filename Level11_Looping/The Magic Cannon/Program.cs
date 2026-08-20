8dcbe0364179cdac44d1667e5c8717f82077ad01
blueblock-no217 <raemunn.tan@gmail.com>
2026-08-20 23:42:31 +0800

    Add files via upload

---
+++ Level11_Looping/The Magic Cannon/Program.cs
@@ -0,0 +1,72 @@
+﻿// 08/05/2026
+
+// Skorin, a member of Consolas’s wall guard, has constructed a magic cannon that draws power from two
+// gems: a fire gem and an electric gem. Every third turn of a crank, the fire gem activates, and the cannon
+// produces a fire blast. The electric gem activates every fifth turn of the crank, and the cannon makes an
+// electric blast. When the two line up, it generates a potent combined blast. Skorin would like your help to
+// produce a program that can warn the crew about which turns of the crank will produce the different
+// blasts before they do it.
+
+// Partial Output 
+/*
+1: Normal
+2: Normal
+3: Fire
+4: Normal
+5: Electric
+6: Fire
+7: Normal
+
+• Write a program that will loop through the values between 1 and 100 and display what kind of blast
+the crew should expect. (The % operator may be of use.)
+• Change the color of the output based on the type of blast. (For example, red for Fire, yellow for
+Electric, and blue for Electric and Fire).
+*/
+
+Console.WriteLine("Every third turn: Fire"); // 
+Console.WriteLine("Every fifth turn: Electric");
+
+int N = 100;
+for (int i = 1; i <= N; i++)
+{
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.ForegroundColor = ConsoleColor.Gray;
+    Console.Write($"{i,-3}: ");
+
+    // Fire and Electric
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write("Fire and Electric");
+    }
+
+    // Fire
+    else if (i % 3 == 0)
+    {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write("Fire");
+    }
+
+    // Electric
+    else if (i % 5 == 0)
+    {
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.Write("Electric");
+    }
+
+    // Normal
+    else 
+    {
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write("Normal");
+    }
+    Console.ResetColor();
+    Console.WriteLine();
+}
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Gray;
---
+++ Level11_Looping/The Prototype/Program.cs
@@ -0,0 +1,84 @@
+﻿/* 07/05/2026
+• Build a program that will allow a user, the pilot, to enter a number.
+• If the number is above 100 or less than 0, keep asking.
+• Clear the screen once the program has collected a good number.
+• Ask a second user, the hunter, to guess numbers.
+• Indicate whether the user guessed too high, too low, or guessed right.
+• Loop until they get it right, then end the program
+ */
+
+// Sets the colors for the text
+// Depending on where it is placed it will paint any changes happening to the screen
+// Paints the console background to yellow and text black
+// Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Clear();
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Press any key to proceed.");
+Console.ReadKey(true); // obtains the key press the user gave
+
+
+Console.WriteLine("User 1, Input a number between 0 - 100 to be guessed: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+
+int num;    // Used in exception handling
+
+// the blcok below is the normal answer
+// check the range of numbers given
+//while(num > 100 || num < 0)
+//{
+//    Console.WriteLine("User 1, input must be within 0 - 100");
+//    num = Convert.ToInt32(Console.ReadLine());
+//}
+
+
+// with the addition of exception handling
+while (true)
+{
+    string user1 = Console.ReadLine();
+    if (!(int.TryParse(user1, out num)))
+    {
+        Console.WriteLine("Invalid, No alphabets.");
+        continue;
+    }
+
+    if (num < 0 || num > 100)
+    {
+        Console.WriteLine("Invalid, numbers between 0 - 100 only.");
+    }
+    break;
+}
+
+
+int guess;
+
+Console.Clear();
+
+Console.WriteLine("User 2, guess a number between 0 - 100: ");
+
+do
+{
+    Console.Write("What is your guess?: ");
+    string input = Console.ReadLine();
+
+    // if the check is successful, this means the input is an integer number
+    // if unsuccessful, it tells the user to input a new number
+    if (!(int.TryParse(input, out guess)))
+    {
+        Console.WriteLine("Invalid, numbers only");
+        continue;
+    }
+
+    if (guess < num)
+    {
+        Console.WriteLine($"{guess} is too Low.");
+    }
+    else if (guess > num)
+    {
+        Console.WriteLine($"{guess} is too High.");
+    }
+}
+while (guess != num);
+
+Console.WriteLine($"You guess correctly, it's {guess}");
